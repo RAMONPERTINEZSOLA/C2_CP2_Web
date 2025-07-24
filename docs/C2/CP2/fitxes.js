3dfd4aec4f35ee66d754319b856cfca802b8fab6
RAMONPERTINEZSOLA <ramon.pertinez@parcsafe.com.au>
2025-07-24 13:32:09 +1000

    Create fitxes.js

---
+++ docs/C2/CP2/fitxes.js
@@ -0,0 +1,73 @@
+addFitxes([
+  { name: "C2_CP2_0_I", path: "C2/CP2/fitxes/C2_CP2_0_I.html" },
+  { name: "C2_CP2_0_II", path: "C2/CP2/fitxes/C2_CP2_0_II.html" },
+  { name: "C2_CP2_0_III", path: "C2/CP2/fitxes/C2_CP2_0_III.html" },
+  { name: "C2_CP2_0_IV", path: "C2/CP2/fitxes/C2_CP2_0_IV.html" },
+  { name: "C2_CP2_0_V", path: "C2/CP2/fitxes/C2_CP2_0_V.html" },
+
+  { name: "C2_CP2_1_I", path: "C2/CP2/fitxes/C2_CP2_1_I.html" },
+  { name: "C2_CP2_1_II", path: "C2/CP2/fitxes/C2_CP2_1_II.html" },
+  { name: "C2_CP2_1_III", path: "C2/CP2/fitxes/C2_CP2_1_III.html" },
+  { name: "C2_CP2_1_IV", path: "C2/CP2/fitxes/C2_CP2_1_IV.html" },
+  { name: "C2_CP2_1_V", path: "C2/CP2/fitxes/C2_CP2_1_V.html" },
+
+  { name: "C2_CP2_2_I", path: "C2/CP2/fitxes/C2_CP2_2_I.html" },
+  { name: "C2_CP2_2_II", path: "C2/CP2/fitxes/C2_CP2_2_II.html" },
+  { name: "C2_CP2_2_III", path: "C2/CP2/fitxes/C2_CP2_2_III.html" },
+  { name: "C2_CP2_2_IV", path: "C2/CP2/fitxes/C2_CP2_2_IV.html" },
+  { name: "C2_CP2_2_V", path: "C2/CP2/fitxes/C2_CP2_2_V.html" },
+
+  { name: "C2_CP2_3_I", path: "C2/CP2/fitxes/C2_CP2_3_I.html" },
+  { name: "C2_CP2_3_II", path: "C2/CP2/fitxes/C2_CP2_3_II.html" },
+  { name: "C2_CP2_3_III", path: "C2/CP2/fitxes/C2_CP2_3_III.html" },
+  { name: "C2_CP2_3_IV", path: "C2/CP2/fitxes/C2_CP2_3_IV.html" },
+  { name: "C2_CP2_3_V", path: "C2/CP2/fitxes/C2_CP2_3_V.html" },
+
+  { name: "C2_CP2_4_I", path: "C2/CP2/fitxes/C2_CP2_4_I.html" },
+  { name: "C2_CP2_4_II", path: "C2/CP2/fitxes/C2_CP2_4_II.html" },
+  { name: "C2_CP2_4_III", path: "C2/CP2/fitxes/C2_CP2_4_III.html" },
+  { name: "C2_CP2_4_IV", path: "C2/CP2/fitxes/C2_CP2_4_IV.html" },
+  { name: "C2_CP2_4_V", path: "C2/CP2/fitxes/C2_CP2_4_V.html" },
+
+  { name: "C2_CP2_5_I", path: "C2/CP2/fitxes/C2_CP2_5_I.html" },
+  { name: "C2_CP2_5_II", path: "C2/CP2/fitxes/C2_CP2_5_II.html" },
+  { name: "C2_CP2_5_III", path: "C2/CP2/fitxes/C2_CP2_5_III.html" },
+  { name: "C2_CP2_5_IV", path: "C2/CP2/fitxes/C2_CP2_5_IV.html" },
+  { name: "C2_CP2_5_V", path: "C2/CP2/fitxes/C2_CP2_5_V.html" },
+
+  { name: "C2_CP2_6_I", path: "C2/CP2/fitxes/C2_CP2_6_I.html" },
+  { name: "C2_CP2_6_II", path: "C2/CP2/fitxes/C2_CP2_6_II.html" },
+  { name: "C2_CP2_6_III", path: "C2/CP2/fitxes/C2_CP2_6_III.html" },
+  { name: "C2_CP2_6_IV", path: "C2/CP2/fitxes/C2_CP2_6_IV.html" },
+  { name: "C2_CP2_6_V", path: "C2/CP2/fitxes/C2_CP2_6_V.html" },
+
+  { name: "C2_CP2_7_I", path: "C2/CP2/fitxes/C2_CP2_7_I.html" },
+  { name: "C2_CP2_7_II", path: "C2/CP2/fitxes/C2_CP2_7_II.html" },
+  { name: "C2_CP2_7_III", path: "C2/CP2/fitxes/C2_CP2_7_III.html" },
+  { name: "C2_CP2_7_IV", path: "C2/CP2/fitxes/C2_CP2_7_IV.html" },
+  { name: "C2_CP2_7_V", path: "C2/CP2/fitxes/C2_CP2_7_V.html" },
+
+  { name: "C2_CP2_8_I", path: "C2/CP2/fitxes/C2_CP2_8_I.html" },
+  { name: "C2_CP2_8_II", path: "C2/CP2/fitxes/C2_CP2_8_II.html" },
+  { name: "C2_CP2_8_III", path: "C2/CP2/fitxes/C2_CP2_8_III.html" },
+  { name: "C2_CP2_8_IV", path: "C2/CP2/fitxes/C2_CP2_8_IV.html" },
+  { name: "C2_CP2_8_V", path: "C2/CP2/fitxes/C2_CP2_8_V.html" },
+
+  { name: "C2_CP2_9_I", path: "C2/CP2/fitxes/C2_CP2_9_I.html" },
+  { name: "C2_CP2_9_II", path: "C2/CP2/fitxes/C2_CP2_9_II.html" },
+  { name: "C2_CP2_9_III", path: "C2/CP2/fitxes/C2_CP2_9_III.html" },
+  { name: "C2_CP2_9_IV", path: "C2/CP2/fitxes/C2_CP2_9_IV.html" },
+  { name: "C2_CP2_9_V", path: "C2/CP2/fitxes/C2_CP2_9_V.html" },
+
+  { name: "C2_CP2_10_I", path: "C2/CP2/fitxes/C2_CP2_10_I.html" },
+  { name: "C2_CP2_10_II", path: "C2/CP2/fitxes/C2_CP2_10_II.html" },
+  { name: "C2_CP2_10_III", path: "C2/CP2/fitxes/C2_CP2_10_III.html" },
+  { name: "C2_CP2_10_IV", path: "C2/CP2/fitxes/C2_CP2_10_IV.html" },
+  { name: "C2_CP2_10_V", path: "C2/CP2/fitxes/C2_CP2_10_V.html" },
+
+  { name: "C2_CP2_11_I", path: "C2/CP2/fitxes/C2_CP2_11_I.html" },
+  { name: "C2_CP2_11_II", path: "C2/CP2/fitxes/C2_CP2_11_II.html" },
+  { name: "C2_CP2_11_III", path: "C2/CP2/fitxes/C2_CP2_11_III.html" },
+  { name: "C2_CP2_11_IV", path: "C2/CP2/fitxes/C2_CP2_11_IV.html" },
+  { name: "C2_CP2_11_V", path: "C2/CP2/fitxes/C2_CP2_11_V.html" }
+]);
