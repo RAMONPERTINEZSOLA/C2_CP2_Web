01aabd4f5faf81500002340e75756b13d7f16926
RAMONPERTINEZSOLA <ramon.pertinez@parcsafe.com.au>
2025-07-28 11:02:19 +1000

    Update fitxes.js

--- docs/E2/fitxes.js
+++ docs/E2/fitxes.js
@@ -1,1 +1,41 @@
+addFitxes([
+  { name: "E2_0_I", path: "E2/fitxes/E2_0_I.html" },
+  { name: "E2_0_II", path: "E2/fitxes/E2_0_II.html" },
+  { name: "E2_0_III", path: "E2/fitxes/E2_0_III.html" },
+  { name: "E2_0_IV", path: "E2/fitxes/E2_0_IV.html" },
+  { name: "E2_0_V", path: "E2/fitxes/E2_0_V.html" },
+  { name: "E2_0_VI", path: "E2/fitxes/E2_0_VI.html" },
 
+  { name: "E2_1_I", path: "E2/fitxes/E2_1_I.html" },
+  { name: "E2_1_II", path: "E2/fitxes/E2_1_II.html" },
+  { name: "E2_1_III", path: "E2/fitxes/E2_1_III.html" },
+  { name: "E2_1_IV", path: "E2/fitxes/E2_1_IV.html" },
+  { name: "E2_1_V", path: "E2/fitxes/E2_1_V.html" },
+  { name: "E2_1_VI", path: "E2/fitxes/E2_1_VI.html" },
+
+  { name: "E2_2_I", path: "E2/fitxes/E2_2_I.html" },
+  { name: "E2_2_II", path: "E2/fitxes/E2_2_II.html" },
+  { name: "E2_2_III", path: "E2/fitxes/E2_2_III.html" },
+  { name: "E2_2_IV", path: "E2/fitxes/E2_2_IV.html" },
+  { name: "E2_2_V", path: "E2/fitxes/E2_2_V.html" },
+  { name: "E2_2_VI", path: "E2/fitxes/E2_2_VI.html" },
+
+  { name: "E2_3_I", path: "E2/fitxes/E2_3_I.html" },
+  { name: "E2_3_II", path: "E2/fitxes/E2_3_II.html" },
+  { name: "E2_3_III", path: "E2/fitxes/E2_3_III.html" },
+  { name: "E2_3_IV", path: "E2/fitxes/E2_3_IV.html" },
+  { name: "E2_3_V", path: "E2/fitxes/E2_3_V.html" },
+  { name: "E2_3_VI", path: "E2/fitxes/E2_3_VI.html" },
+
+  { name: "E2_4_I", path: "E2/fitxes/E2_4_I.html" },
+  { name: "E2_4_II", path: "E2/fitxes/E2_4_II.html" },
+
+  { name: "E2_5_I", path: "E2/fitxes/E2_5_I.html" },
+  { name: "E2_5_II", path: "E2/fitxes/E2_5_II.html" },
+
+  { name: "E2_6_I", path: "E2/fitxes/E2_6_I.html" },
+  { name: "E2_6_II", path: "E2/fitxes/E2_6_II.html" },
+
+  { name: "E2_7_I", path: "E2/fitxes/E2_7_I.html" },
+  { name: "E2_7_II", path: "E2/fitxes/E2_7_II.html" }
+]);
